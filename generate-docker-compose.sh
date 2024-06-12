f45cbe19d1a5a7988b2f4547a6b84b5559aca2b0
chpark-ML <bch0322@gmail.com>
2024-06-12 12:52:05 +0000

    updates

--- generate-docker-compose.sh
+++ generate-docker-compose.sh
@@ -1,4 +1,9 @@
 # script.sh
 #!/bin/bash
-export $(grep -v '^#' .env | xargs)
+
+# .env 파일을 읽어서 환경 변수로 설정
+while IFS= read -r line; do
+    export "$line"
+done < .env
+
 envsubst < docker-compose.template.yaml > $DOCKER_COMPOSE_NAME
